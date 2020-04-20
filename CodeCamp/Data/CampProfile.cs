441e6bafc7e214fd9a2ca58609f46580df4ffb3f
Edgar Santiago <itt.edgarstgo@gmail.com>
2020-04-20 19:08:53 -0400

    Association APIs

--- CodeCamp/Data/CampProfile.cs
+++ CodeCamp/Data/CampProfile.cs
@@ -12,9 +12,18 @@ namespace CodeCamp.Data
     {
         public CampProfile()
         {
+
             this.CreateMap<Camp, CampModel>()
-              .ForMember(c => c.Venue, o => o.MapFrom(m => m.Location.VenueName))
-              .ReverseMap();
+                .ForMember(c => c.Venue, o => o.MapFrom(m => m.Location.VenueName))
+                .ReverseMap();
+
+            this.CreateMap<Talk, TalkModel>()
+                .ReverseMap()
+                .ForMember(t => t.Camp, opt => opt.Ignore())
+                .ForMember(t => t.Speaker, opt => opt.Ignore());
+
+            this.CreateMap<Speaker, SpeakerModel>()
+                .ReverseMap();
         }
     }
 }
